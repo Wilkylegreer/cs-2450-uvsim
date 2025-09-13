5f70773b658740d166bbd989cbf8ceb159ff734c
darbannette <51731207+darbannette@users.noreply.github.com>
2025-09-13 08:56:04 -0600

    Update io_handler.py
    Sets memory size to 100, loads file and cleans lines of comments and white space and returns cleaned lines. 
    
    the run_program function is only operational for reading and writing and breaks if any other opcode is entered

--- io_handler.py
+++ io_handler.py
@@ -1,1 +1,28 @@
 #io.py
+
+memorySize = 100
+def load_program(filename):
+    memory = [0] * memorySize
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+
+    for i, line in enumerate(lines):
+        clean = line.split("//")[0].strip()
+        if clean:
+            memory[i] = int(clean)
+    return memory
+
+
+def run_program(memory):
+    counter = 0
+    while True:
+        instruction = memory[counter]
+        operation = instruction // 100
+        memLocation = instruction % 100
+        if operation == 10:
+            memory[memLocation] = int(input(f"Type a word: [{memLocation}]: "))
+        elif operation == 11:
+            print(memory[memLocation]) #Write to screen
+        else:
+            break
+        counter += 1
