ebb0486c087816260d3bf5456113e6e366a8175a
darbannette <51731207+darbannette@users.noreply.github.com>
2025-09-17 09:51:35 -0600

    Update math_instructions.py

--- math_instructions.py
+++ math_instructions.py
@@ -1,1 +1,21 @@
-#instructions.py
+#math_instructions.py
+
+class MathInstructions():
+    def __init__(self, memory):
+        self.memory = memory
+      
+    def add():
+        self.accumulator += self.memory[operand]
+
+    def subtract():
+      self.accumulator -= self.memory[operand]
+
+    def divide():
+      if self.memory[operand] == 0:
+        print("can't divide by zero")
+        break
+      self.accumulator //= self.memory[operand]
+  
+    def multiply():
+      self.accumulator *= self.memory[operand]
+      
