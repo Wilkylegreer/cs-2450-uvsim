ff5cbfeb702b35cd5c0135c3b8a7e699bc41edc1
ejrass <ethanjras@gmail.com>
2025-09-18 14:43:50 -0600

    Integrated Darbys updates - replaced address with memoryLoc

--- math_instructions.py
+++ math_instructions.py
@@ -12,18 +12,32 @@ class MathInstructions():
         self.memory = memory
 
 
-    def EXECUTE(self, opcode, accumulator, memoryLoc, memory):
+    def EXECUTE(self, opcode, accumulator, memoryLoc):
         operation = self.OPCODE_DICT.get(opcode)
 
         if not operation:
             raise ValueError(f"Unknown mathematical operation: {opcode}")
         
         if operation == "ADD":
-            return self.add(self, opcode, accumulator, memoryLoc, memory)
+            return self.add(self, accumulator, memoryLoc)
         elif operation == "SUBTRACT":
-            return self.subtract(self, opcode, accumulator, memoryLoc, memory)
+            return self.subtract(self, accumulator, memoryLoc)
         elif operation == "DIVIDE":
-            return self.divide(self, opcode, accumulator, memoryLoc, memory)
+            return self.divide(self, accumulator, memoryLoc)
         elif operation == "MULTIPLY":
-            return self.multiply(self, opcode, accumulator, memoryLoc, memory)
-        
+            return self.multiply(self, accumulator, memoryLoc)
+    
+    def add(self, accumulator, memoryLoc):
+        accumulator += self.memory.mem[memoryLoc]
+
+    def subtract(self, accumulator, memoryLoc):
+        accumulator -= self.memory.mem[memoryLoc]
+
+    def divide(self, accumulator, memoryLoc):
+        if self.memory[memoryLoc] == 0:
+            #print("can't divide by zero")
+            return ValueError("Cannot divide by zero!")
+        accumulator //= self.memory.mem[memoryLoc]
+  
+    def multiply(self, accumulator, memoryLoc):
+        accumulator *= self.memory.mem[memoryLoc]
