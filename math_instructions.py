efdc7e809f8762fae959f79c3d48cdb4a1c2637d
darbannette <51731207+darbannette@users.noreply.github.com>
2025-09-19 01:39:45 -0600

    Update math_instructions.py

--- math_instructions.py
+++ math_instructions.py
@@ -4,19 +4,19 @@ class MathInstructions():
     def __init__(self, memory):
         self.memory = memory
       
-    def add(self, accumulator, memory):
+    def add(self, address):
         self.cpu.accumulator += self.memory.mem[address]
 
-    def subtract(self, accumulator, memory):
+    def subtract(self, address):
       self.cpu.accumulator -= self.memory.mem[address]
 
-    def divide(self, accumulator, memory):
+    def divide(self, address):
       if self.memory[address] == 0:
         print("can't divide by zero")
         break
       self.cpu.accumulator //= self.memory.mem[address]
   
-    def multiply(self, accumulator, memory):
+    def multiply(self, address):
       self.cpu.accumulator *= self.memory.mem[address]
 
 
