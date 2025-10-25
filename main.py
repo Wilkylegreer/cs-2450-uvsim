87dcb579f413d179460d2a75c70812afe3562a08
wilkye <wilkylegreer@gmail.com>
2025-10-24 21:53:45 -0600

    Removed redundant code in main.py

--- main.py
+++ main.py
@@ -7,33 +7,5 @@ def main():
     app = UvsimGUI(root)
     root.mainloop()
 
-    # Initializations
-    # memory = Memory()
-    # cpu = CPU(memory)
-
-    # conInstruct = ControlInstructions(memory, cpu)
-    # mathInstruct = MathInstructions(memory)
-
-    # cpu.set_instructions(conInstruct, mathInstruct)
-
-    # proLoader = ProgramLoader(memory)
-
-    # app.program_loaded = False
-    # app.log_message("Test1")
-    # if hasattr(app, "selected_file"):
-    #     try:
-    #         app.program_loaded = proLoader.load_from_file(app.selected_file)
-    #         if not app.program_loaded:
-    #             print(f"Error loading file '{app.selected_file}': Invalid program")
-    #     except Exception as e:
-    #         print(f"Error loading file '{app.selected_file}': {e}")
-
-    # # Start if loaded right
-    # if app.program_loaded:
-    #     cpu.run()
-
-    # print(".")
-
-
 if __name__ == "__main__":
     main()
